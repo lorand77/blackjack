815a2d4f2f6fe406dea264c59c21db0ec24a50c4
lorand <lorand77@users.noreply.github.com>
2024-08-21 18:39:11 -0500

    complete game

--- blackjack.py
+++ blackjack.py
@@ -3,14 +3,9 @@ import random
 ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 rank_values = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
 suits = ["♡", "♢", "♠", "♣"]
-deck = []
 
 chips = 100
 bet = 10
-
-for r in ranks:
-    for s in suits:
-        deck.append((r, s))
 
 def print_cards(cards):
     for card in cards:
@@ -46,22 +41,74 @@ def hand_value(hand):
 def is_blackjack(hand):
     return len(hand) == 2 and hand_value(hand) == 21
 
+def game_round():
+    global chips
 
-random.shuffle(deck)
+    deck = []
+    for r in ranks:
+        for s in suits:
+            deck.append((r, s))
+    random.shuffle(deck)
 
-dealer_hand = []
-player_hand = []
+    dealer_hand = []
+    player_hand = []
 
-dealer_hand.append(deal_card(deck))
-player_hand.append(deal_card(deck))
-player_hand.append(deal_card(deck))
-
-print("Dealer: ",end="")
-print_cards(dealer_hand)
-print("Player: ",end="")
-print_cards(player_hand)
-
-if is_blackjack(player_hand):
     dealer_hand.append(deal_card(deck))
-    if not is_blackjack(dealer_hand):
-        chips += bet * 3 // 2
+    player_hand.append(deal_card(deck))
+    player_hand.append(deal_card(deck))
+
+    print("Dealer: ",end="")
+    print_cards(dealer_hand)
+    print("Player: ",end="")
+    print_cards(player_hand)
+
+    if is_blackjack(player_hand):
+        dealer_hand.append(deal_card(deck))
+        if not is_blackjack(dealer_hand):
+            chips += bet * 3 // 2
+            print(">>> Player wins with Blackjack!")
+        else:
+            print(">>> Draw with player and dealer Blackjack!")
+        return
+    
+    while True:
+        while True:
+            ans = input("Hit or stand (h/s)? ")
+            if ans == "h" or ans == "s":
+                break
+        if ans == "s":
+            break
+        player_hand.append(deal_card(deck))
+        print("Player: ",end="")
+        print_cards(player_hand)
+        if hand_value(player_hand) >= 21:
+            break
+    
+    if hand_value(player_hand) > 21:
+        chips -= bet
+        print(">>> Player busted!")
+        return
+    
+    while hand_value(dealer_hand) < 17:
+        dealer_hand.append(deal_card(deck))
+        print("Dealer: ",end="")
+        print_cards(dealer_hand)
+
+    if hand_value(dealer_hand) > 21:
+        chips += bet
+        print(">>> Dealer busted!")
+        return
+
+    if hand_value(player_hand) > hand_value(dealer_hand):
+        chips += bet
+        print(">>> Player wins!")
+    elif hand_value(player_hand) < hand_value(dealer_hand):
+        chips -= bet
+        print(">>> Dealer wins!")
+    else:
+        print(">>> Draw!")
+
+for i in range(10):
+    game_round()
+    print(chips)
+    print("--------------------------------------------------------")
