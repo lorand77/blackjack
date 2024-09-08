c43080f175c23a000c9fa59b6112b1de9c69e407
lorand <lorand77@users.noreply.github.com>
2024-09-08 10:47:44 +0200

    fix

--- card_counting/simulation.py
+++ card_counting/simulation.py
@@ -6,10 +6,7 @@ rank_values = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, 
 suits = ["♡", "♢", "♠", "♣"]
 
 NUMBER_OF_DECKS = 6
-count_history = [0]
-
-def deal_card(deck):
-    return deck.pop(0)
+running_count_history = [0]
 
 deck = []
 for r in ranks:
@@ -20,11 +17,11 @@ random.shuffle(deck)
 
 for card in deck:
     if card[0] == "A" or rank_values[card[0]] > 9:
-        count_history.append(count_history[-1] -1)
+        running_count_history.append(running_count_history[-1] -1)
     elif rank_values[card[0]] < 7:
-        count_history.append(count_history[-1] +1)
+        running_count_history.append(running_count_history[-1] +1)
     else:
-        count_history.append(count_history[-1])
+        running_count_history.append(running_count_history[-1])
 
 x = list(range(0,52*NUMBER_OF_DECKS+1))
 for i in range(0,len(x)):
@@ -34,7 +31,7 @@ for i in range(0,len(x)):
 
 true_count_history = []
 for i in range(0,52*NUMBER_OF_DECKS+1):
-    true_count_history.append(count_history[i]/((52*NUMBER_OF_DECKS-i)//52+1))
+    true_count_history.append(running_count_history[i]/((52*NUMBER_OF_DECKS-i)//52+1))
     #true_count_history.append((52*NUMBER_OF_DECKS-i)//52+1)
 
 plt.plot(x[:int(52*NUMBER_OF_DECKS*0.75)],true_count_history[:int(52*NUMBER_OF_DECKS*0.75)])
